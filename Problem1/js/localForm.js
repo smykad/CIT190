b8dd00e41b29509a69dfb1ea9a6769f01e72215e
Doug <70054506+smykad@users.noreply.github.com>
2022-04-01 19:56:01 -0400

    commit

---
+++ Problem1/js/localForm.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("sub").addEventListener("click",function(){
+	localStorage.firstname = document.getElementById("firstName").value;
+	localStorage.lastname = document.getElementById("lastName").value;
+	localStorage.email = document.getElementById("emailAddress").value;
+	localStorage.phone = document.getElementById("phone").value;
+	window.location.assign("localConfirm.html");
+});
