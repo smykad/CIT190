1afd45891edd56a03e03dff301814a7a07bc0a59
Doug <70054506+smykad@users.noreply.github.com>
2022-04-17 03:35:42 -0400

    commit

--- finalproject/draganddrop/js/dnd.js
+++ finalproject/draganddrop/js/dnd.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
 
     var w = window.innerWidth*.8;
     var h = window.innerWidth*.4;
-    var c = w*.1;
+    var c = w*.15;
     
     // adjust the size of the mygame object
     $("#myGame").html('<object width="' + w + '" height="' + h + '"/>');
-    $('#myGame').prepend('<img class="drag" src="media/pirate.png"' + c + '" height="' + c + '" />');
-    $('#myGame').append('<img class="drag" src="media/pirate.png"' + c + '" height="' + c + '" />');
-    
+    $('#myGame').prepend('<img class="drag" src="media/bonclay.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/brook.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/chopper.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/franky.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/luffy.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/nami.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/sanji.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/sogeking.png"' + c + '" height="' + c + '" />');
+    $('#myGame').prepend('<img class="drag" src="media/zoro.png"' + c + '" height="' + c + '" />');
     $('.drag').draggable({
         containment: "#myGame"
     });
